c4f4a0ccdd251f0fa94f1426068f1657aeeb38b9
Hexuar <hexuar@gmail.com>
2025-09-11 11:51:47 +0200

    Put print under physics namespace

--- physics.h
+++ physics.h
@@ -739,16 +739,17 @@ inline std::ostream& physics::operator<<(std::ostream& os, const unit& u) {
 #include <iostream>
 
 
-inline void print() {
-    std::cout << std::endl;
-};
+namespace physics {
+    inline void print() {
+        std::cout << std::endl;
+    };
 
-template <typename T, typename... Types>
-void print(T var1, Types...var2) {
-    std::cout << var1 << "\t";
-    print(var2...);
+    template <typename T, typename... Types>
+    void print(T var1, Types...var2) {
+        std::cout << var1 << "\t";
+        print(var2...);
+    }
 }
-
 
 // end --- print.h --- 
 
--- src/print.h
+++ src/print.h
@@ -3,12 +3,14 @@
 #include <iostream>
 
 
-inline void print() {
-    std::cout << std::endl;
-};
+namespace physics {
+    inline void print() {
+        std::cout << std::endl;
+    };
 
-template <typename T, typename... Types>
-void print(T var1, Types...var2) {
-    std::cout << var1 << "\t";
-    print(var2...);
+    template <typename T, typename... Types>
+    void print(T var1, Types...var2) {
+        std::cout << var1 << "\t";
+        print(var2...);
+    }
 }
